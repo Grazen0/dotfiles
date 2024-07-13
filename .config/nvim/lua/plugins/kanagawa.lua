de301b5380b3698a484d67c8d700d6639dd00098
ElCholoGamer <josedanielgrayson@gmail.com>
2024-07-13 12:28:57 -0500

    nvim(kanagawa): change some configs

--- .config/nvim/lua/plugins/kanagawa.lua
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -10,17 +10,23 @@ return {
 				},
 			},
 		},
-		compile = true,
-		transparent = false,
+		transparent = true,
 		keywordStyle = { italic = false },
 		functionStyle = { italic = false },
 		overrides = function(colors)
 			local theme = colors.theme
 
 			return {
-				TelescopeTitle = { fg = theme.ui.special, bold = true },
+				NormalFloat = { bg = 'none' },
+				FloatBorder = { bg = 'none' },
+				FloatTitle = { bg = 'none' },
+				NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
 
-				Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+				Pmenu = {
+					fg = theme.ui.shade0,
+					bg = theme.ui.bg_p1,
+					blend = vim.o.pumblend,
+				},
 				PmenuSel = { fg = 'NONE', bg = theme.ui.bg_p2 },
 				PmenuSbar = { bg = theme.ui.bg_m1 },
 				PmenuThumb = { bg = theme.ui.bg_p2 },
@@ -31,7 +37,7 @@ return {
 		require('kanagawa').setup(opts)
 
 		vim.cmd('colorscheme kanagawa')
-		-- vim.cmd('highlight TelescopeBorder guibg=none')
-		-- vim.cmd('highlight TelescopeTitle guibg=none')
+		vim.cmd('highlight TelescopeBorder guibg=none')
+		vim.cmd('highlight TelescopeTitle guibg=none')
 	end,
 }
