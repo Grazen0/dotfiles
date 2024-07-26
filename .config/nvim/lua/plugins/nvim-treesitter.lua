7bce9f8cd2ae7b3eaa8b3864a5add6ae7f7d125c
ElCholoGamer <josedanielgrayson@proton.me>
2024-07-25 22:12:00 -0500

    nvim(treesitter): add filetype support for hyprland config

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -10,5 +10,9 @@ return {
 	},
 	config = function(_, opts)
 		require('nvim-treesitter.configs').setup(opts)
+
+		vim.filetype.add({
+			pattern = { ['.*/hypr/.*%.conf'] = 'hyprlang' },
+		})
 	end,
 }
