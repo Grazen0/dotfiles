fa87b36cb94991cb7fc45d61d5e59812a735e9db
ElCholoGamer <josedanielgrayson@proton.me>
2024-08-01 00:06:03 -0500

    nvim(vimtex): revert vimtex_quickfix_mode to its default value 2

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -2,9 +2,7 @@ return {
 	'lervag/vimtex',
 	lazy = false,
 	init = function()
-		vim.g.tex_flavor = 'latex'
 		vim.g.vimtex_view_method = 'zathura'
-		vim.g.vimtex_quickfix_mode = 0
 		vim.g.vimtex_compiler_latexmk = {
 			out_dir = 'dist',
 		}
